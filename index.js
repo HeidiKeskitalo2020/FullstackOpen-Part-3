0bb16778eaa3f855cb4c27826cdbfbdc1d392dbd
Heidi <info@risako.fi>
2020-10-08 13:21:32 +0300

    EX.3.4 Done

--- index.js
+++ index.js
@@ -41,13 +41,20 @@ app.get(`/api/persons/`, (req, res) => {
 
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
+    const person = persons.find(person => person.id === id)
 
     if (person) {
         res.json(person)
     } else {
-        res.status(404).emd()
+        res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
 })
   
   const PORT = 3001
