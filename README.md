b91f37e7dbaaae683ff139ff89c2e2a0b7c561a5
Heidi <info@risako.fi>
2020-10-09 14:24:47 +0300

    Heroku added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FullstackOpen-Part-3
+Here is a [link](http://98655.herokuapp.com/api/persons) to the backend in exercise 3.10.
+
+https://enigmatic-shelf-98655.herokuapp.com/
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const cors = require('corse')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
