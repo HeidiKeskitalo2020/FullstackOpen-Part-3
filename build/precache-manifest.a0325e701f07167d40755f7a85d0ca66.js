a0cc661f138660380dccad9c4ee180a9d7f414cf
Heidi <info@risako.fi>
2020-10-12 13:05:56 +0300

    production build

---
+++ build/precache-manifest.a0325e701f07167d40755f7a85d0ca66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077596a150bda85ee919c0a6553cecf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afee93b9818c4be84a6d",
+    "url": "/static/css/main.72e76ab6.chunk.css"
+  },
+  {
+    "revision": "10822974f48efc347430",
+    "url": "/static/js/2.badec770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.badec770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afee93b9818c4be84a6d",
+    "url": "/static/js/main.3b94aba5.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
